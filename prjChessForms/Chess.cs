e31a33d8772dfc2f4cefaffa688a1403ac608cb8
Shinglington <4kshing@gmail.com>
2023-02-15 16:25:55 +0000

    Creates instance of board and displays cells

---
+++ prjChessForms/Chess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjChessForms
+{
+    public partial class Chess : Form
+    {
+        private Board _board;
+        private Player[] _players;
+        private int _currentTurn;
+        public Chess()
+        {
+            InitializeComponent();
+            CreatePlayers();
+            CreateBoard();
+        }
+
+
+        private void CreatePlayers()
+        {
+            _players = new Player[2];
+            _players[0] = new Player();
+            _players[1] = new Player();
+        }
+
+        private void CreateBoard()
+        {
+            TableLayoutPanel boardTable = new TableLayoutPanel()
+            {
+                Parent = this,
+                Dock = DockStyle.Fill
+            };
+
+            _board = new Board(boardTable, _players);
+        }
+    }
+}
