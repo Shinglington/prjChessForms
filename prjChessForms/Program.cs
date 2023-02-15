cbc3139cc2c46c2d16f2b0b2c3604447570bd47a
Shinglington <4kshing@gmail.com>
2023-02-15 16:26:19 +0000

    Empty classes Piece and Player

--- prjChessForms/Program.cs
+++ prjChessForms/Program.cs
@@ -16,7 +16,7 @@ namespace prjChessForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Chess());
         }
     }
 }
