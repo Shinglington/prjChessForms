4c842f8e28aceecbec74da5a17e9bbdb98d8d0a3
Shinglington <4kshing@gmail.com>
2023-02-17 12:52:15 +0000

    Move Struct and Human and Computer players

--- prjChessForms/Player.cs
+++ prjChessForms/Player.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace prjChessForms
 {
-    class Player
+
+    public struct Move 
+    {
+        private Coords _startCoords;
+        private Coords _endCoords;
+
+        public Move(Coords startCoords, Coords endCoords)
+        {
+            _startCoords = startCoords;
+            _endCoords = endCoords;
+        }
+
+        public Coords StartCoords { get { return _startCoords; } }
+        public Coords EndCoords { get { return _endCoords; } }
+    }
+
+
+
+    abstract class Player
     {
         private PieceColour _colour;
         public Player(PieceColour colour)
@@ -22,6 +40,18 @@ namespace prjChessForms
             }
         }
 
-
+        public 
     }
+
+    class HumanPlayer : Player
+    {
+        public HumanPlayer(PieceColour colour) : base(colour) { }
+    }
+
+    class ComputerPlayer : Player 
+    { 
+        public ComputerPlayer(PieceColour colour) : base(colour) { }
+    }
+
+
 }
