9f62cea4029cd7a873ec9de3167c87115e7f9b58
Kai Shing <kshing21@students.psc.ac.uk>
2023-02-27 16:12:10 +0000

    Added GetPossibleMoves in rulebook

--- prjChessForms/Rulebook.cs
+++ prjChessForms/Rulebook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -40,6 +41,29 @@ namespace prjChessForms
         public static bool CheckIfGameOver(Board board, Player playerTurn)
         {
             return false;
+        }
+
+
+        public static List<Coords> GetPossibleMoves(Board board, Coords pieceCoords)
+        {
+            List<Coords> possibleMoves = new List<Coords>();
+            if (board.GetPieceAt(pieceCoords) != null)
+            {
+                Piece piece = board.GetPieceAt(pieceCoords);
+                Coords checkingCoords;
+                for(int y = 0; y < board.RowCount; y++)
+                {
+                    for (int x = 0; x < board.ColumnCount; x++)
+                    {
+                        checkingCoords = new Coords(x, y);
+                        if (CheckLegalMove(board, piece.Owner, pieceCoords, checkingCoords)) 
+                        {
+                            possibleMoves.Add(checkingCoords);
+                        }
+                    }
+                }
+            }
+            return possibleMoves;
         }
 
 
