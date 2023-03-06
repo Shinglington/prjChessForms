22cb59b05c194f7cc0a3a54b84373425ef13eed1
Shinglington <4kshing@gmail.com>
2023-03-06 18:46:13 +0000

    Moved Square to new file

--- prjChessForms/MyChessLibrary/Board.cs
+++ prjChessForms/MyChessLibrary/Board.cs
@@ -4,8 +4,6 @@ using System.Drawing;
 
 namespace prjChessForms.MyChessLibrary
 {
-
-
     class Board
     {
         private const int ROW_COUNT = 8;
@@ -199,39 +197,5 @@ namespace prjChessForms.MyChessLibrary
         }
     }
 
-    class Square
-    {
-        public EventHandler PieceChanged;
-
-        private Piece _piece;
-        public Square(int x, int y)
-        {
-            Coords = new Coords(x, y);
-            Piece = null;
-        }
-
-        public Piece Piece
-        {
-            get
-            {
-                if (_piece != null && _piece.GetType() == typeof(GhostPawn))
-                {
-                    return null;
-                }
-                return _piece;
-            }
-            set
-            {
-                _piece = value;
-                PieceChanged.Invoke(this, EventArgs.Empty);
-            }
-        }
-        public Coords Coords { get; }
-        public GhostPawn GetGhostPawn()
-        {
-            return (_piece != null && _piece.GetType() == typeof(GhostPawn)) ? (GhostPawn)_piece : null;
-        }
-
-    }
 
 }
---
+++ prjChessForms/MyChessLibrary/Square.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace prjChessForms.MyChessLibrary
+{
+    class Square
+    {
+        public EventHandler PieceChanged;
+        private Piece _piece;
+        public Square(int x, int y)
+        {
+            Coords = new Coords(x, y);
+            Piece = null;
+        }
+        public Piece Piece
+        {
+            get
+            {
+                if (_piece != null && _piece.GetType() == typeof(GhostPawn))
+                {
+                    return null;
+                }
+                return _piece;
+            }
+            set
+            {
+                _piece = value;
+                PieceChanged.Invoke(this, EventArgs.Empty);
+            }
+        }
+        public Coords Coords { get; }
+
+        public GhostPawn GetGhostPawn()
+        {
+            return (_piece != null && _piece.GetType() == typeof(GhostPawn)) ? (GhostPawn)_piece : null;
+        }
+    }
+}
