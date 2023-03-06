f99c4e0f262b3a56e6f762663f45102973e6d7a4
Shinglington <4kshing@gmail.com>
2023-03-06 18:45:20 +0000

    Moved Coords struct to new file

--- prjChessForms/MyChessLibrary/Board.cs
+++ prjChessForms/MyChessLibrary/Board.cs
@@ -5,39 +5,7 @@ using System.Drawing;
 namespace prjChessForms.MyChessLibrary
 {
 
-    public struct Coords
-    {
-        public Coords(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-        public int X { get; }
-        public int Y { get; }
 
-        public override string ToString() => $"{(char)('a' + X)}{Y + 1}";
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null || obj.GetType() != typeof(Coords))
-            {
-                return false;
-            }
-            else
-            {
-                Coords other = (Coords)obj;
-                return other.X == X && other.Y == Y;
-            }
-        }
-
-        public override int GetHashCode()
-        {
-            int hashCode = 367829482;
-            hashCode = hashCode * -1521134295 + X.GetHashCode();
-            hashCode = hashCode * -1521134295 + Y.GetHashCode();
-            return hashCode;
-        }
-    }
     class Board
     {
         private const int ROW_COUNT = 8;
---
+++ prjChessForms/MyChessLibrary/Coords.cs
@@ -0,0 +1,36 @@
+﻿namespace prjChessForms.MyChessLibrary
+{
+   public struct Coords
+    {
+        public Coords(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int X { get; }
+        public int Y { get; }
+
+        public override string ToString() => $"{(char)('a' + X)}{Y + 1}";
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != typeof(Coords))
+            {
+                return false;
+            }
+            else
+            {
+                Coords other = (Coords)obj;
+                return other.X == X && other.Y == Y;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 367829482;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            return hashCode;
+        }
+    }
+}
