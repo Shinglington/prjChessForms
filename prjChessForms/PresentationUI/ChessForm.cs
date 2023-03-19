1117a1384f6522d7360ac65f191f3cc85e8a29d3
Shinglington <4kshing@gmail.com>
2023-03-19 16:37:15 +0000

    Added PlayerInformationPanel class

--- prjChessForms/PresentationUI/ChessForm.cs
+++ prjChessForms/PresentationUI/ChessForm.cs
@@ -12,6 +12,9 @@ namespace prjChessForms.PresentationUI
 
         private BoardTableLayoutPanel _boardPanel;
         private TableLayoutPanel _layoutPanel;
+
+        private PlayerInformationPanel _whiteInfo;
+        private PlayerInformationPanel _blackInfo;
         public ChessForm()
         {
             InitializeComponent();
@@ -40,7 +43,13 @@ namespace prjChessForms.PresentationUI
 
         private void SetupControls()
         {
-            // layout
+            SetupLayoutPanel();
+            SetupBoard();
+            SetupPlayerInfo();
+        }
+
+        private void SetupLayoutPanel()
+        {
             _layoutPanel = new TableLayoutPanel()
             {
                 Parent = this,
@@ -55,9 +64,10 @@ namespace prjChessForms.PresentationUI
             _layoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 5));
             _layoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 90));
             _layoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 5));
+        }
 
-
-            // Board panel
+        private void SetupBoard()
+        {
             _boardPanel = new BoardTableLayoutPanel(this)
             {
                 Parent = _layoutPanel,
@@ -65,64 +75,12 @@ namespace prjChessForms.PresentationUI
             };
             _boardPanel.SquareClicked += OnBoardClicked;
             _layoutPanel.SetCellPosition(_boardPanel, new TableLayoutPanelCellPosition(0, 1));
+        }
 
-            // Timer
-            //_timer = new System.Timers.Timer(1000);
-            //_timerLabels = new Label[2];
-
-
-            //// White player label
-            //TableLayoutPanel whiteTable = new TableLayoutPanel()
-            //{
-            //    Parent = _layoutPanel,
-            //    Dock = DockStyle.Fill,
-            //    ColumnStyles = { new ColumnStyle(SizeType.Percent, 50), new ColumnStyle(SizeType.Percent, 50) },
-            //    RowStyles = { new RowStyle(SizeType.Percent, 100) },
-            //};
-            //_layoutPanel.SetCellPosition(whiteTable, new TableLayoutPanelCellPosition(0, 2));
-            //Label whiteLabel = new Label()
-            //{
-            //    Parent = whiteTable,
-            //    Dock = DockStyle.Fill,
-            //    Text = _players[0].Colour.ToString(),
-            //};
-            //whiteTable.SetCellPosition(whiteLabel, new TableLayoutPanelCellPosition(0, 0));
-            //_timerLabels[0] = new Label()
-            //{
-            //    Parent = whiteTable,
-            //    Dock = DockStyle.Fill,
-            //    Text = _players[0].RemainingTime.ToString(),
-            //};
-            //whiteTable.SetCellPosition(_timerLabels[0], new TableLayoutPanelCellPosition(1, 0));
-
-
-
-
-
-            // Black player label
-            //TableLayoutPanel blackTable = new TableLayoutPanel()
-            //{
-            //    Parent = _layoutPanel,
-            //    Dock = DockStyle.Fill,
-            //    ColumnStyles = { new ColumnStyle(SizeType.Percent, 50), new ColumnStyle(SizeType.Percent, 50) },
-            //    RowStyles = { new RowStyle(SizeType.Percent, 100) },
-            //};
-            //_layoutPanel.SetCellPosition(blackTable, new TableLayoutPanelCellPosition(0, 0));
-            //Label blackLabel = new Label()
-            //{
-            //    Parent = blackTable,
-            //    Dock = DockStyle.Fill,
-            //    Text = _players[1].Colour.ToString(),
-            //};
-            //blackTable.SetCellPosition(blackLabel, new TableLayoutPanelCellPosition(0, 0));
-
-            //_timerLabels[1] = new Label()
-            //{
-            //    Parent = blackTable,
-            //    Dock = DockStyle.Fill,
-            //    Text = _players[1].RemainingTime.ToString(),
-            //};
-            //whiteTable.SetCellPosition(_timerLabels[1], new TableLayoutPanelCellPosition(1, 0));
+        private void SetupPlayerInfo()
+        {
+            _whiteInfo = new PlayerInformationPanel(PieceColour.White);
+            _blackInfo = new PlayerInformationPanel(PieceColour.Black);
         }
 
         private void OnBoardClicked(object sender, SquareClickedEventArgs e)
--- prjChessForms/PresentationUI/PlayerInformationPanel.cs
+++ prjChessForms/PresentationUI/PlayerInformationPanel.cs
@@ -1,12 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using prjChessForms.MyChessLibrary;
+using System;
+using System.Windows.Forms;
 
 namespace prjChessForms.PresentationUI
 {
-    internal class PlayerInformationPanel
+    class PlayerInformationPanel : TableLayoutPanel
     {
+        private Label _colourLabel;
+        private Label _timeLabel;
+        private FlowLayoutPanel _capturedPieces;
+        public PlayerInformationPanel(PieceColour colour)
+        {
+            PieceColour = colour;
+            SetupPanel();
+        }
+
+        public PieceColour PieceColour { get; }
+
+        private void SetupPanel()
+        {
+            RowStyles.Clear();
+            ColumnStyles.Clear();
+            
+            RowStyles.Add(new RowStyle(SizeType.Percent, 100));
+            ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20));
+            ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 60));
+            ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20));
+
+            SetupLabel();
+            SetupCapturedPieces();
+            SetupTimer();
+
+        }
+
+        private void SetupLabel()
+        {
+            _colourLabel = new Label 
+            {
+                Text = PieceColour.ToString(),
+                Parent = this,
+                Dock = DockStyle.Fill,
+            };
+            SetCellPosition(_colourLabel, new TableLayoutPanelCellPosition(0, 0));
+        }
+
+        private void SetupCapturedPieces()
+        {
+            _capturedPieces = new FlowLayoutPanel() 
+            {
+                FlowDirection = FlowDirection.LeftToRight,
+                Parent = this,
+                Dock = DockStyle.Fill,
+            };
+            SetCellPosition(_capturedPieces, new TableLayoutPanelCellPosition(1, 0));
+
+        }
+
+        private void SetupTimer()
+        {
+            _timeLabel = new Label()
+            {
+                Text = TimeSpan.Zero.ToString(),
+                Parent = this,
+                Dock = DockStyle.Fill,
+            };
+            SetCellPosition(_timeLabel, new TableLayoutPanelCellPosition(2, 0));
+        }
+
+
+
+
     }
 }
