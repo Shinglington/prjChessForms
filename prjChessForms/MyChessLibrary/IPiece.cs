bf45d80410fc1759d7e67d0c051f5b243cad1bea
Shinglington <4kshing@gmail.com>
2023-07-04 22:32:34 +0100

    Piece Interface

---
+++ prjChessForms/MyChessLibrary/IPiece.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace prjChessForms.MyChessLibrary
+{
+    interface IPiece
+    {
+        bool HasMoved { get; set; }
+        Image Image { get; }
+        PieceColour Colour { get; }
+        string Name { get; }
+        string Fullname { get; } 
+        bool CanMove(Board board, Coords startCoords, Coords endCoords);
+    }
+}
